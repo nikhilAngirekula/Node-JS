1df9ff6f9d9587d58249a7110a4642289e0aa097
Nikhil Angirekula <nikhilangirekula@nikhils-MacBook-Pro.local>
2020-04-12 14:40:32 +0530

    Now we can edit the product and add the product
    Edit and add and replace the product

--- Node Udemy/controllers/shop.js
+++ Node Udemy/controllers/shop.js
@@ -1,11 +1,12 @@
-const Product = require("../models/product");
+const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render("shop/product-list", {
+    res.render('shop/product-list', {
       prods: products,
-      pageTitle: "All Products",
-      path: "/products"
+      pageTitle: 'All Products',
+      path: '/products'
     });
   });
 };
@@ -13,41 +14,49 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
-    res.render("shop/product-detail", {
+    res.render('shop/product-detail', {
+      product: product,
       pageTitle: product.title,
-      path: "/products",
-      product: product
+      path: '/products'
     });
   });
 };
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render("shop/index", {
+    res.render('shop/index', {
       prods: products,
-      pageTitle: "Shop",
-      path: "/"
+      pageTitle: 'Shop',
+      path: '/'
     });
   });
 };
 
 exports.getCart = (req, res, next) => {
-  res.render("shop/cart", {
-    path: "/cart",
-    pageTitle: "Your Cart"
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
+  });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
   });
 };
 
 exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", {
-    path: "/checkout",
-    pageTitle: "Checkout"
-  });
-};
-
-exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders"
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
   });
 };
--- Node Udemy/models/product.js
+++ Node Udemy/models/product.js
@@ -1,10 +1,10 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  "data",
-  "products.json"
+  'data',
+  'products.json'
 );
 
 const getProductsFromFile = cb => {
@@ -18,20 +18,32 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, price, description) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
-    this.price = price;
     this.description = description;
+    this.price = price;
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
+      if (this.id) {
+        const existingProductIndex = products.findIndex(
+          prod => prod.id === this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
     });
   }
 
