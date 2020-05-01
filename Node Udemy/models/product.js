775945b3fd0e611b9127158d770b6b53e26344c2
Nikhil Angirekula <nikhilangirekula@nikhils-MacBook-Pro.local>
2020-05-01 16:10:07 +0530

    Started MongoDb
    Deleted the product in the mongoDb.

--- Node Udemy/models/product.js
+++ Node Udemy/models/product.js
@@ -1,27 +1,74 @@
-const Sequelize = require('sequelize');
+const mongoDb = require("mongoDb");
+const getDb = require("../util/database").getDb;
 
-const sequelize = require('../util/database');
-
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
+class Product {
+  constructor(title, price, imageUrl, description, id) {
+    this.title = title;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this._id = new mongoDb.ObjectID(id);
   }
-});
+
+  save() {
+    const db = getDb();
+    let dbOp;
+    if (this._id) {
+      //Update the product
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      // Insert the product
+      dbOp = db.collection("products").insertOne(this);
+    }
+    return dbOp.then().catch((err) => {
+      console.log(err);
+    });
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        console.log(products);
+        return products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: new mongoDb.ObjectId(prodId) })
+      .next()
+      .then((product) => {
+        console.log(product);
+        return product;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongoDb.ObjectID(prodId) })
+      .then((result) => {
+        console.log("Deleted");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
 
 module.exports = Product;
--- Node Udemy/util/database.js
+++ Node Udemy/util/database.js
@@ -1,19 +1,31 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://Nikhil_Nick:fTYtWLQvW3DM4fNQ@cluster0-e5hxu.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://Nikhil_Nick:fTYtWLQvW3DM4fNQ@cluster0-e5hxu.mongodb.net/shop?retryWrites=true&w=majority"
   )
     .then((client) => {
       console.log("Connected!");
+      _db = client.db();
       callback(client);
     })
     .catch((err) => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No data base found";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
 
 // fTYtWLQvW3DM4fNQ
